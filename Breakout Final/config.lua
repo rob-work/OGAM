f753400333fbb67eaa4c5d64fb33dad68b292ca6
Rob Englebright <r.englebright@jisc.ac.uk>
2014-01-30 11:46:24 +0000

    Update config.lua
    set defaults to iPad2 screen

--- Breakout Final/config.lua
+++ Breakout Final/config.lua
@@ -2,10 +2,10 @@ application =
 {
         content =
         {
-            graphicsCompatibility = 1,
-            width = 320,
-            height = 480, 
-            scale = "letterbox",
+            graphicsCompatibility = 1, --Old code needs this to cope with new Graphics2 framework
+            width = 768,--default size for iPad2- not sure what sort of device should be min spec?
+            height = 1024, --even though game is landscape this needs to indicate width/ height as portrait.
+            scale = "letterbox", -- interesting to see what happens with 5S long screen
             fps = 60,
         },
 }
